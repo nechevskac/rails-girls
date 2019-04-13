550e32b078908cf09835c0896b7f2d91bddf4c28
ceca <cecanechevska@yahoo.com>
2019-04-13 12:49:05 +0200

    Added picture and new controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'pages/info'
   resources :ideas
   get 'welcome/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
